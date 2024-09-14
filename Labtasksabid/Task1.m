255467c339bdf5b12cfc535127f587be969a5996
Anindya Kishore Choudhury <anindyakchoudhury@gmail.com>
2024-09-14 13:26:30 +0600

    TaskA1 Done

--- Labtasksabid/Task1.m
+++ Labtasksabid/Task1.m
@@ -1,43 +1,42 @@
 clc;
-clear;
+clearvars;
 close all;
 format long;
 set(0,'DefaultAxesFontName', 'Latex');
 set(0,'DefaultAxesFontSize', 13);
 
 %% Loading data
-data = importdata('GaAs2.txt');
+data   = importdata('GaAs2.txt');
 lambda = data(:,1);
 lambda = lambda.*1e-6; %m
-n = data(:,2);
-k = data(:,3);
-m0=9.11e-31;
-q=1.6e-19;
-eps0=8.854e-12;
+n      = data(:,2);
+k      = data(:,3);
+m0     = 9.11e-31;
+q      = 1.6e-19;
+eps0   = 8.854e-12;
 
 %% physical constants
-me      = 0.063*m0;
-mh      = 0.5*m0;
-mr      = me*mh/(me+mh);
-h=6.624e-34;
-hcut=h/(2*pi);
-c=3e8;
-Eg=1.42*q;
-fcv     = 23*q;
-fcvf    = fcv/1000;
+me     = 0.063*m0;
+mh     = 0.5*m0;
+mr     = me*mh/(me+mh);
+h      = 6.624e-34;
+hcut   = h/(2*pi);
+c      = 3e8;
+Eg     = 1.42*q;
+fcv    = 23*q;
+fcvf   = fcv/100;
+eps    = 12.9*eps0;
 
-eps     = 12.9*eps0;
+nr     = mean(n); %added from abid's memory
 
-nr = mean(n); %added from abid's memory
+%% Lab Task 1: Compare 'alpha' from 'k' with 'alpha' obtained from formula (using 'n')
 
-%% Lab Task 1: Compare 'alpha' from 'k' with 'alpha' obtained from formula (using 'n') 
-
-E= h*c./lambda;
+E       = h*c./lambda;
 % allowed transition
 alpha   = q^2*sqrt(m0)./(4*pi*hcut^2*eps*c.*n).*(2*mr/m0)^1.5 .*(fcv./E).*(E-Eg).^0.5;
 
 % forbidden transition
-alphaf   = q^2*sqrt(m0)/(6*pi*hcut^2*eps*c*nr) ...
+alphaf  = q^2*sqrt(m0)/(6*pi*hcut^2*eps*c*nr) ...
             .*(2*mr/m0)^2.5 .*(fcvf./E).*(E-Eg).^1.5;  %1/m
 
 
@@ -45,6 +44,7 @@ figure(1);
 % yyaxis left;
 subplot(131)
 plot(lambda/1e-6, alpha/100,'Linewidth', 1.5);
+xlabel ('\lambda (\mum)');
 ylabel('\alpha  cm^{-1}');
 title('Direct Allowed Transition');
 hold on;
@@ -59,8 +59,6 @@ title(sprintf("Comparison of \\alpha for %s", 'GaAs'));
 title('Experimental Transition');
 
 
-%legend('Theoretical \alpha','Experimental \alpha = 4\pi\kappa /\lambda');
-
 % figure(2)
 subplot(133)
 % Direct Forbidden Transition
@@ -71,9 +69,3 @@ ylabel('\alpha_f  cm^{-1}');
 title('Direct Forbidden Transition');
 subtitle(sprintf(string));
 grid on;
-
-% subplot(122)
-% plot(E./q, alphaf/100,'Linewidth', 1.5);
-% xlabel ('h\nu (eV)');
-% ylabel('\alpha_f cm^{-1}');
-% grid on;
--- Labtasksabid/TaskB.m
+++ Labtasksabid/TaskB.m
@@ -6,38 +6,38 @@ set(0,'DefaultAxesFontName', 'Latex');
 set(0,'DefaultAxesFontSize', 13);
 
 %% Loading data
-data = importdata('GaAs2.txt');
+data   = importdata('GaAs2.txt');
 lambda = data(:,1);
 lambda = lambda.*1e-6; %m
-n = data(:,2);
-k = data(:,3);
-m0=9.11e-31;
-q=1.6e-19;
-eps0=8.854e-12;
+n      = data(:,2);
+k      = data(:,3);
+m0     = 9.11e-31;
+q      = 1.6e-19;
+eps0   = 8.854e-12;
 
 
 %% physical constants
-me      = 0.063*m0;
-mh      = 0.5*m0;
-mr      = me*mh/(me+mh);
-h=6.624e-34;
-hcut=h/(2*pi);
-c=3e8;
-Eg=1.42*q;
-fcv     = 23*q;
-fcvf    = fcv/1000;
-kB      = 1.38e-23;
-eps     = 12.9*eps0;
-T = 300; 
+me     = 0.063*m0;
+mh     = 0.5*m0;
+mr     = me*mh/(me+mh);
+h      = 6.624e-34;
+hcut   = h/(2*pi);
+c      = 3e8;
+Eg     = 1.42*q;
+fcv    = 23*q;
+fcvf   = fcv/1000;
+kB     = 1.38e-23;
+eps    = 12.9*eps0;
+T      = 300;
 
 %% Numerically calculate the total spontaneous emission rate
 E       = h*c./lambda;
 
 % allowed transition
 % nr for fixed refractive index
-% n for dispersion 
+% n for dispersion
 alpha   = q^2*sqrt(m0)./(4*pi*hcut^2*eps*c.*n) ...
-            .*(2*mr/m0)^1.5 .*(fcv./E).*sqrt((E-Eg));   
+            .*(2*mr/m0)^1.5 .*(fcv./E).*sqrt((E-Eg));
 alpha = real(alpha);
 P = alpha .* (c./n);
 phi = 8*pi.*(E/h).^3.*n.^3./c^3   .*  (1./(exp(E/(kB*T)) - 1));
@@ -62,17 +62,17 @@ grid on;
 
 % %% Experimental comparison
 % % Experimental spectra from k
-% alpha_exp   = 4*pi*k./lambda;   
-% 
+% alpha_exp   = 4*pi*k./lambda;
+%
 % alpha_exp = real(alpha_exp);
 % P = alpha_exp .* (c./n);
 % phi = 8*pi.*(E/h).^3.*n.^3./c^3   .*  (1./(exp(E/(kB*T)) - 1));
-% 
-% 
-% 
+%
+%
+%
 % rsp = P.*phi;
-% 
-% 
+%
+%
 % figure(2);
 % subplot(211);
 % plot(lambda/1e-9,real(rsp)/100,'Linewidth', 1.5);
@@ -80,29 +80,28 @@ grid on;
 % ylabel('R_{sp} (1/cm)');
 % title('alpha = 4\pi k/\lambda');
 % grid on;
-% 
-% 
+%
+%
 % subplot(212)
 % plot(E/q,real(rsp)/100,'Linewidth', 1.5)
 % xlabel('E (eV)');
 % ylabel('R_{sp} (1/cm)');
 % grid on;
-% 
-% 
-% 
+%
+%
 % %% Rsp for different Temperature
 % %  interp1(E,alpha,,method)
-% 
-% nr      = mean(n);   
+%
+% nr      = mean(n);
 % Ts = 200:10:250;
 % A = 0.54/1000;
 % B = 204;
 % lambda = linspace(200,1000,10000)*1e-9;
 % E_ = h*c./lambda;
-% 
+%
 % figure(3);
 % for T = Ts
-%     
+%
 %     Eg_ = (1.519 - A*T^2/(B+T))*q;
 %     alpha = q^2*sqrt(m0)./(4*pi*hcut^2*eps*c.*nr) ...
 %             .*(2*mr/m0)^1.5 .*(fcv./E_).*sqrt((E_-Eg_));
